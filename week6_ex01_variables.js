515a2ded881412c3910d1e303910b44d74da2d6d
ShuaibDarries <shuaibdarries7298@gmail.com>
2026-05-19 12:27:31 +0200

    Add variables for personal information and introduction

---
+++ week6_ex01_variables.js
@@ -0,0 +1,24 @@
+// TODO: Variable called firstName with my first name stored in it
+let firstName = "Shuaib";
+console.log(firstName);
+
+// TODO: Variable called lastName with my last name stored in it
+let lastName = "Darries";
+console.log(lastName);
+
+// TODO: Variable called age with my age stored in it
+let age = 21;
+console.log(age);
+
+// TODO: Variable called fullName that combines my first and last name
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// TODO: Variable called introduction that combines my full name and age to introduce myself
+let introduction =
+  "Hello, my name is " + fullName + " and I am " + age + " years old.";
+console.log(introduction);
+
+// TODO: Changing the age variable to a different value
+age = 22;
+console.log(age);
